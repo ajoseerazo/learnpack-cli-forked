97f1c52207c8aed65df1fe281c9ac18bb5a10347
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2021-08-12 15:33:04 -0400

    Functions added to utils. doRequest() changed for node-fetch

--- src/commands/audit.js
+++ src/commands/audit.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
 const request = require('request');
-const linkCheck = require('link-check');
+const fetch = require('node-fetch')
 const { flags } = require('@oclif/command')
 const Console = require('../utils/console')
+const { isUrl, findInFile, showErrors, showWarnings } = require('../utils/audit')
 const SessionCommand = require('../utils/SessionCommand');
 const fm = require("front-matter")
 
@@ -17,7 +18,7 @@ class AuditCommand extends SessionCommand {
         Console.log("Running command audit...")
 
         // Build exercises if they are not built yet.
-        if(!this.configManager.get().exercises) this.configManager.buildIndex()
+        if (!this.configManager.get().exercises) this.configManager.buildIndex()
 
         // Get configuration object.
         const config = this.configManager.get();
@@ -25,84 +26,15 @@ class AuditCommand extends SessionCommand {
         let errors = []
         let warnings = []
 
-        // This function checks if a url is valid.
-        async function isUrl(url){
-            let regex_url = /(https?:\/\/[a-zA-Z_\-.\/0-9]+)/gm
-            if(!regex_url.exec(url)) {
-                errors.push({exercise: null, msg: `The repository value of the configuration file is not a link: ${url}`})
-                return false;
-            }
-            doRequest(url).catch((err) => errors.push(errors.push({exercise: null, msg: `The link of the repository is broken: ${url}`})))
-            return true;
-        }
-        
-        const findInFile = (types, content) => {
-            
-            const regex = {
-                relative_images: /!\[.*\]\s*\(((\.\/)?(\.{2}\/){1,5})(.*\.[a-zA-Z]{2,4}).*\)/gm,
-                external_images: /!\[.*\]\((https?:\/(\/{1}[^/)]+)+\/?)\)/gm,
-                markdown_links: /(\s)+\[.*\]\((https?:\/(\/{1}[^/)]+)+\/?)\)/gm,
-                url: /(https?:\/\/[a-zA-Z_\-.\/0-9]+)/gm,
-                uploadcare: /https:\/\/ucarecdn.com\/(?:.*\/)*([a-zA-Z_\-.\/0-9]+)/gm
-            }
-
-            const validTypes = Object.keys(regex);
-            if (!Array.isArray(types)) types = [types];
-
-            let findings = {}
-
-            types.forEach(type => {
-                if (!validTypes.includes(type)) throw Error("Invalid type: " + type)
-                else findings[type] = {};
-            });
-            
-            types.forEach(type => {
-                
-                let count = 0;
-                let m;
-                while ((m = regex[type].exec(content)) !== null) {
-                    // This is necessary to avoid infinite loops with zero-width matches
-                    if (m.index === regex.lastIndex) {
-                        regex.lastIndex++;
-                    }
-
-                    // The result can be accessed through the `m`-variable.
-                    // m.forEach((match, groupIndex) => values.push(match));
-                    count++;
-                    
-                    findings[type][m[0]] = {
-                        content: m[0],
-                        absUrl: m[1],
-                        mdUrl: m[2],
-                        relUrl: m[4]
-                    }
-                }
-            })
-
-            return findings;
-        }
-
-        // This function does the request to check if the images, videos and links are working.
-        function doRequest(url) {
-            return new Promise(function (resolve, reject) {
-                request(url, function (error, res, body) {
-                    if (!error && res.statusCode == 200) {
-                        resolve(body);
-                    } else {
-                        reject(error)
-                    }
-                });
-            });
-        }
-
         // This function checks that each of the url's are working.
-        async function checkUrl(file, exercise) {
+        const checkUrl = async (file, exercise) => {
             if (!fs.existsSync(file.path)) return false
             const content = fs.readFileSync(file.path).toString();
             const frontmatter = fm(content).attributes
             for (const attribute in frontmatter) {
                 if (attribute === "intro" || attribute === "tutorial") {
-                    await doRequest(frontmatter[attribute]).catch((err) => errors.push({exercise: exercise, msg: `This link is broken: ${frontmatter[attribute]}`}))
+                    let res = await fetch(frontmatter[attribute], { method: "HEAD" });
+                    if (!res.ok) errors.push({ exercise: exercise, msg: `This link is broken (${res.ok}): ${frontmatter[attribute]}` })
                 }
             }
 
@@ -114,106 +46,76 @@ class AuditCommand extends SessionCommand {
                     // Valdites all the relative path images.
                     for (const img in obj) {
                         // Validates if the path is correct
-                        if (obj[img].absUrl !== "../../" || obj[img].absUrl !== "./../../") errors.push({exercise: exercise, msg: `The path for this image (${obj[img].relUrl}) is incorrect`})
-                        
+                        if (obj[img].absUrl !== "../../" || obj[img].absUrl !== "./../../") errors.push({ exercise: exercise, msg: `The path for this image (${obj[img].relUrl}) is incorrect` })
+
                         // Validates if the image is in the assets folder.
-                        if (!fs.existsSync(obj[img].relUrl)) errors.push({exercise: exercise, msg: `The file ${obj[img].relUrl} doesn't exist in the assets folder.`})
+                        if (!fs.existsSync(obj[img].relUrl)) errors.push({ exercise: exercise, msg: `The file ${obj[img].relUrl} doesn't exist in the assets folder.` })
                     }
                 } else if (finding === "external_images" && Object.keys(obj).length > 0) {
                     // Valdites all the aboslute path images.
                     for (const img in obj) {
-                        await doRequest(obj[img].absUrl).catch((err) => errors.push({exercise: exercise, msg: `This link is broken: ${obj[img].absUrl}`}))
+                        let res = await fetch(obj[img].absUrl, { method: "HEAD" });
+                        if (!res.ok) errors.push({ exercise: exercise, msg: `This link is broken: ${obj[img].absUrl}` })
                     }
                 } else if (finding === "markdown_links" && Object.keys(obj).length > 0) {
                     // This was working but linkCheck, stopped working
                     for (const link in obj) {
-                        await doRequest(obj[link].mdUrl).catch((err) => errors.push({exercise: exercise, msg: `This link is broken: ${obj[link].mdUrl}`}))
+                        let res = await fetch(obj[link].mdUrl, { method: "HEAD" });
+                        if (!res.ok) errors.push({ exercise: exercise, msg: `This link is broken: ${obj[link].mdUrl}` })
                     }
                 }
             }
             return true
         }
-        
+
         // This function is being created because the find method doesn't work with promises.
-        async function find(file, lang, exercise){
-            if(file.name === lang){
+        const find = async (file, lang, exercise) => {
+            if (file.name === lang) {
                 await checkUrl(file, exercise)
                 return true
             }
             return false
         }
-        
-        // This function checks if there are errors, and show them in the console at the end.
-        function showErrors(errors) {
-            return new Promise((resolve, reject) => {
-                if (errors) {
-                    if (errors.length > 0) {
-                        Console.log("Checking for errors...")
-                        errors.forEach((error, i) => Console.error(`${i + 1}) ${error.msg} ${error.exercise != null ? `(Exercise: ${error.exercise})` : ""}`))
-                        process.exit(1)
-                    } else {
-                        Console.success("We didn't find any errors in this repository.")
-                        process.exit(0)
-                    }
-                    resolve("SUCCESS")
-                } else {
-                    reject("Failed")
-                }
-            })
-        }
 
-        // This function checks if there are warnings, and show them in the console at the end.
-        function showWarnings(warnings) {
-            return new Promise((resolve, reject) => {
-                if (warnings) {
-                    if (warnings.length > 0) {
-                        Console.log("Checking for warnings...")
-                        warnings.forEach((warning, i) => Console.warning(`${i + 1}) ${warning}`))
-                    }
-                    resolve("SUCCESS")
-                } else {
-                    reject("Failed")
-                }
-            })
-        }
+
 
         // These two lines check if the 'slug' property is inside the configuration object.
         Console.debug("Checking if the slug property is inside the configuration object...")
-        if (!config.slug) errors.push({exercise: null, msg: "The slug property is not in the configuration object"})
+        if (!config.slug) errors.push({ exercise: null, msg: "The slug property is not in the configuration object" })
 
         // These two lines check if the 'repository' property is inside the configuration object.
         Console.debug("Checking if the repository property is inside the configuration object...")
-        if (!config.repository) errors.push({exercise: null, msg: "The repository property is not in the configuration object"})
-        else isUrl(config.repository)
+        if (!config.repository) errors.push({ exercise: null, msg: "The repository property is not in the configuration object" })
+        else isUrl(config.repository, errors)
 
         // These two lines check if the 'description' property is inside the configuration object.
         Console.debug("Checking if the description property is inside the configuration object...")
-        if (!config.description) errors.push({exercise: null, msg: "The description property is not in the configuration object"})
-        
+        if (!config.description) errors.push({ exercise: null, msg: "The description property is not in the configuration object" })
+
         // Validates if images and links are working at every README file.
         let exercises = config.exercises
         let readmeFiles = []
 
-        if(exercises.length > 0){
-            for(const index in exercises){
+        if (exercises.length > 0) {
+            for (const index in exercises) {
                 let exercise = exercises[index]
                 let readmeFilesCount = 0;
-                if(Object.keys(exercise.translations).length == 0) errors.push({exercise: exercise.title, msg: `The exercise ${exercise.title} doesn't have a README.md file.`})
-                
-                for(const lang in exercise.translations){
+                if (Object.keys(exercise.translations).length == 0) errors.push({ exercise: exercise.title, msg: `The exercise ${exercise.title} doesn't have a README.md file.` })
+
+                for (const lang in exercise.translations) {
                     let files = []
-                    for(const file of exercise.files){
+                    for (const file of exercise.files) {
                         let found = await find(file, exercise.translations[lang], exercise.title)
-                        if(found == true) readmeFilesCount++
+                        if (found == true) readmeFilesCount++
                         files.push(found)
                     }
-                    if(!files.includes(true)) errors.push({exercise: exercise.title, msg: `This exercise doesn't have a README.md file.`})
+                    if (!files.includes(true)) errors.push({ exercise: exercise.title, msg: `This exercise doesn't have a README.md file.` })
 
                 }
                 readmeFiles.push(readmeFilesCount)
             }
-        } else errors.push({exercise: null, msg: "The exercises array is empty."})
-        
+        } else errors.push({ exercise: null, msg: "The exercises array is empty." })
+
         // Check if all the exercises has the same ammount of README's, this way we can check if they have the same ammount of translations.
         if (!readmeFiles.every((item, index, arr) => item == arr[0])) warnings.push(`Some exercises are missing translations.`)
 
