78be98f1d8a9b640da6da3896309397f810b07a8
Amilcar Erazo <ajose.erazo@gmail.com>
2021-11-20 17:24:29 -0500

    Adding more tests

--- test/unit/utils/validators.test.ts
+++ test/unit/utils/validators.test.ts
@@ -1,7 +1,23 @@
-import * as assert from 'assert'
+// eslint-disable-next-line
+const assert = require('assert');
+import {validURL} from '../../../src/utils/validators'
+
+const EXAMPLE_URL = 'https://example.com'
+const GITHUB_URL = 'https://github.com'
 
 describe('validURL', () => {
-  it('should return false if is a github url', () => {
-    assert.equal([1, 2, 3].indexOf(4), -1)
+  it('should return false if param url is not a github url', () => {
+    const isValid = validURL(EXAMPLE_URL)
+    assert.equal(isValid, false)
+  })
+
+  it('should return false is param url is not a valid url', () => {
+    const isValid = validURL('whatever')
+    assert.equal(isValid, false)
+  })
+
+  it('should return true if param url is a valid github url', () => {
+    const isValid = validURL(GITHUB_URL)
+    assert.equal(isValid, true)
   })
 })
