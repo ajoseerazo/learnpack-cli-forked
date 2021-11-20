691aca95be418dc6eca01b11905670a2990472bf
Amilcar Erazo <ajose.erazo@gmail.com>
2021-11-19 20:16:59 -0500

    WIP

---
+++ src/utils/validators.ts
@@ -0,0 +1,18 @@
+export function validURL(str: string) {
+  if (!str.includes('github.com')) {
+    return false
+  }
+
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i',
+  ) // fragment locator
+  return !!pattern.test(str)
+}
+
+export default {validURL}
